a23a8db49314ea07c51eecd62de5a120b9fc28b6
Himanshu Nag <himanshu_nag@yahoo.com>
2019-03-06 00:27:10 +0530

    First Commit
    Upload project

---
+++ Assets/CameraController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+public class CameraController : MonoBehaviour
+{
+    private Transform startMarker, endMarker;
+    public List<Transform> waypoints;
+    int currentStartPoint;
+    public float speed = 1.0F;
+    private float startTime;
+    private float journeyLength;
+    public float smooth = 5.0F;
+
+    public bool isLooping = false;
+    [Tooltip("Set this to 0 if you dont want wait time.")]
+    public float waitingTime;
+ 
+    void Start()
+    {
+        //Add an instance of first position as last position so loop can happen
+        if(isLooping){
+            waypoints.Add(waypoints[0]);
+        }
+        currentStartPoint = 0;
+        SetPoints();
+    }
+    /// <summary>
+    /// Sets the next cube as destination.
+    /// </summary>
+    void SetPoints()
+    {
+        startMarker = waypoints[currentStartPoint];
+        endMarker = waypoints[currentStartPoint + 1];
+        startTime = Time.time;
+        journeyLength = Vector3.Distance(startMarker.position, endMarker.position);
+        if (isLooping && currentStartPoint + 2 == waypoints.Count)
+        {
+            currentStartPoint = -1;
+        }
+    }
+    /// <summary>
+    /// Update this instance.
+    /// </summary>
+    void Update()
+    {
+        float distCovered = (Time.time - startTime) * speed;
+        float fracJourney = distCovered / journeyLength;
+        //Starts the lerping with current position and desitnation position.
+        transform.position = Vector3.Lerp(startMarker.position, endMarker.position, fracJourney);
+        //Starts lerping rotation of the camera from current rotation to destination rotation
+        transform.rotation = Quaternion.Lerp(startMarker.rotation, endMarker.rotation, fracJourney);
+        if (fracJourney >= 1f && currentStartPoint + 2 < waypoints.Count)
+        {
+            //Updates the current position
+            currentStartPoint++;
+            //Will call SetPoints to set the next destination after waitingTime amount of seconds.If waiting time is 0, then will call instantly.
+            Invoke("SetPoints",waitingTime);
+        }
+    }
+    /// <summary>
+    /// Draws blue line connecting cubes in Editor mode.
+    /// </summary>
+    [ExecuteInEditMode]
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        for (int x = 0; x < waypoints.Count - 1; x++)
+        {
+            Vector3 from = new Vector3(waypoints[x].position.x, waypoints[x].position.y, waypoints[x].position.z);
+            Vector3 to = new Vector3(waypoints[x + 1].position.x, waypoints[x + 1].position.y, waypoints[x + 1].position.z);
+            Gizmos.DrawLine(from, to);
+        }
+
+    }
+}
---
+++ Assets/CameraControllerEditor.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditorInternal;
+
+[CustomEditor(typeof(CameraController), true, isFallback = true)]
+public class CameraControllerEditor : Editor
+{
+
+    CameraController cameraController;
+    private ReorderableList _myList;
+    SerializedProperty m_speed;
+    SerializedProperty m_smooth;
+    SerializedProperty m_isLooping;
+    SerializedProperty m_isWaittime;
+    public void OnEnable()
+    {
+
+        cameraController = (CameraController)target;
+
+        _myList = new ReorderableList(serializedObject, serializedObject.FindProperty("waypoints"), true, true, true, true);
+
+        _myList.drawHeaderCallback = rect =>
+        {
+            EditorGUI.LabelField(rect, "Set Waypoints", EditorStyles.boldLabel);
+        };
+
+        _myList.drawElementCallback =
+            (Rect rect, int index, bool isActive, bool isFocused) =>
+            {
+                var element = _myList.serializedProperty.GetArrayElementAtIndex(index);
+                EditorGUI.ObjectField(new Rect(rect.x, rect.y, rect.width, EditorGUIUtility.singleLineHeight), element, GUIContent.none);
+            };
+    }
+
+    public override void OnInspectorGUI()
+    {
+        m_speed = serializedObject.FindProperty("speed");
+        m_smooth = serializedObject.FindProperty("smooth");
+        m_isWaittime = serializedObject.FindProperty("waitingTime");
+        m_isLooping = serializedObject.FindProperty("isLooping");
+        serializedObject.Update();
+        EditorGUILayout.Slider(m_speed, 0, 100, new GUIContent("Speed"));
+        EditorGUILayout.Slider(m_smooth, 0, 100, new GUIContent("Smooth"));
+        EditorGUILayout.Slider(m_isWaittime, 0, 5, new GUIContent("Wait Time"));
+        EditorGUILayout.PropertyField(m_isLooping, new GUIContent("Loop"));
+        _myList.DoLayoutList();
+        serializedObject.ApplyModifiedProperties();
+    }
+}
